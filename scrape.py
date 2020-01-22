ef050c586afea9b19ff4a2e155415d3b780e59c6
lct45 <leah.thomas144@gmail.com>
2020-01-22 16:57:12 -0500

    blind functionality

--- scrape.py
+++ scrape.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import operator
 
-def urlScraper(url):
+def urlScraperReddit(url):
     response = requests.get(url, timeout=3)
     content = BeautifulSoup(response.content, "html.parser")
     #print(content)
     print("puled content")
     dictionary = {}
-    for texting in content.findAll('p', class_="_1qeIAgB0cPwnLhDF9XSiJM"):
+    test = content.findAll('p', class_="_1qeIAgB0cPwnLhDF9XSiJM")
+    print("here")
+    for item in test:
+        print(item)
+    for texting in test:
         print("in first loop")
         if texting is None:
             print("LETS DANGING CONTINUE")
@@ -22,11 +27,34 @@ def urlScraper(url):
                 dictionary[char]=+1
                 print(char)
 
+def urlScraperBlind(url):
+    response = requests.get(url, timeout=3)
+    content = BeautifulSoup(response.content, "html.parser")
+    #print(content)
+    dictionary = {}
+    test = content.find_all('div', class_="detail")
+    for item in test:
+        print(type(item))
+        temp = item.text
+        #print(temp)
+        #print(type(temp))
+        for words in temp.split():
+            print(words)
+            if words not in dictionary:
+                dictionary[words] = 1
+            else:
+                dictionary[words]+=1
+                #print(char) 
 
+    sorted_x = sorted(dictionary.items(), key=operator.itemgetter(1))
+    for k, v in sorted_x:
+        print("{}                       {}".format(k, v))
 
 
 def main():
-   urlScraper('https://www.reddit.com/r/cscareerquestions/comments/edqtrq/i_21f_poc_have_a_really_hard_time_fitting_in_bc/')
+   #urlScraperReddit('https://www.reddit.com/r/cscareerquestions/comments/edqtrq/i_21f_poc_have_a_really_hard_time_fitting_in_bc/')
+   urlScraperBlind('https://www.teamblind.com/post/Micarried-during-critical-deadline-BTtgJgJD')
+
 
 
 
